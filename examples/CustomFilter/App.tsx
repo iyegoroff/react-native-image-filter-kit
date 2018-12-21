3ec9eb936803626dd18de9f59093276b3bbd8a1c
Igor Yegorov <iegoroff@gmail.com>
2018-12-21 14:10:05 +0300

    custom filter example

---
+++ examples/CustomFilter/App.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import { View } from 'react-native'
+
+const app = () => (
+  <View style={{ backgroundColor: 'red', width: '100%', height: '100%' }} />
+)
+
+export default app
