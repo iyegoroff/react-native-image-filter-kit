3c1bb2992f004692c04f9c2143ca54b5f674dc76
Igor Yegorov <iegoroff@gmail.com>
2019-08-20 03:52:01 +0300

    ios composition fix

--- ios/IFKCompositionPostProcessor.m
+++ ios/IFKCompositionPostProcessor.m
@@ -4,10 +4,9 @@
 
 @interface IFKPostProcessor ()
 
-- (nonnull UIImage *)filteredImageWithScale:(CGFloat)scale
-                                 resizeMode:(RCTResizeMode)resizeMode
-                                  viewFrame:(CGRect)viewFrame
-                                   destSize:(CGSize)destSize;
+- (nonnull UIImage *)filteredImage:(UIImage *)image
+                        resizeMode:(RCTResizeMode)resizeMode
+                         viewFrame:(CGRect)viewFrame;
 - (nonnull CIFilter *)filter;
 - (void)initFilter:(CGSize)size;
 - (nonnull NSDictionary*)inputs;
@@ -35,7 +34,7 @@
                                                          translate:CGPointMake(0.5f, 0.5f)
                                                              scale:[[IFKScaleWithMode alloc] initWithMode:COVER]
                                                             rotate:0];
-    
+
     _swapImages = [[IFKInputConverter convertBoolean:[[self inputs] objectForKey:@"swapImages"]
                                         defaultValue:@(NO)] boolValue];
     _resizeCanvasTo = [IFKInputConverter convertText:[[self inputs] objectForKey:@"resizeCanvasTo"]
@@ -46,7 +45,7 @@
                                            defaultValue:transform];
     _canvasSize = canvasSize;
   }
-  
+
   return self;
 }
 
@@ -58,12 +57,12 @@
 {
   CGFloat width = 0;
   CGFloat height = 0;
-  
+
   if ([transform.scale isKindOfClass:[IFKScaleWithMode class]]) {
     IFKScaleMode mode = ((IFKScaleWithMode *)transform.scale).mode;
     CGFloat bitmapAspect = bitmapWidth / bitmapHeight;
     CGFloat canvasAspect = canvasWidth / canvasHeight;
-    
+
     if (mode == CONTAIN) {
       if (bitmapAspect < canvasAspect) {
         height = canvasHeight;
@@ -72,7 +71,7 @@
         width = canvasWidth;
         height = bitmapHeight * width / bitmapWidth;
       }
-      
+
     } else if (mode == COVER) {
       if (bitmapAspect < canvasAspect) {
         width = canvasWidth;
@@ -81,35 +80,35 @@
         height = canvasHeight;
         width = bitmapWidth * height / bitmapHeight;
       }
-      
+
     } else if (mode == STRETCH) {
       width = canvasWidth;
       height = canvasHeight;
     }
-    
+
   } else if ([transform.scale isKindOfClass:[IFKScaleWithSize class]]) {
     width = canvasWidth * ((IFKScaleWithSize *) transform.scale).scale.x;
     height = canvasHeight * ((IFKScaleWithSize *) transform.scale).scale.y;
   }
-  
+
   CGRect f = CGRectMake(canvasWidth * transform.translate.x - width * transform.anchor.x,
                         canvasHeight * transform.translate.y - height * transform.anchor.y,
                         width,
                         height);
-  
+
   CGAffineTransform t = CGAffineTransformMakeScale(f.size.width / bitmapWidth, f.size.height / bitmapHeight);
   t = CGAffineTransformConcat(t, CGAffineTransformMakeTranslation(-f.size.width * transform.anchor.x, -f.size.height * transform.anchor.y));
   t = CGAffineTransformConcat(t, CGAffineTransformMakeRotation(-transform.rotate));
   t = CGAffineTransformConcat(t, CGAffineTransformMakeTranslation(f.size.width * transform.anchor.x, f.size.height * transform.anchor.y));
   t = CGAffineTransformConcat(t, CGAffineTransformMakeTranslation(f.origin.x, f.origin.y));
-  
+
   return t;
 }
 
 - (nonnull NSString *)dstImageName
 {
   static NSArray<NSString *> *dstImageNames;
-  
+
   static dispatch_once_t onceToken;
   dispatch_once(&onceToken, ^{
     dstImageNames = @[
@@ -122,17 +121,17 @@
       @"inputShadingImage"
     ];
   });
-  
+
   NSArray *inputKeys = [[self filter] inputKeys];
-  
+
   NSString *name = [dstImageNames reduce:^id(id acc, NSString *val, int idx) {
     return [inputKeys containsObject:val] ? val : acc;
   } init:@""];
-  
+
   RCTAssert(![name isEqualToString:@""],
             @"ImageFilterKit: unknown filter input - %@",
             [self filter].name);
-  
+
   return name;
 }
 
@@ -143,25 +142,25 @@
   if (_resizeCanvasTo == nil) {
     return defaultExtent;
   }
-  
+
   if ([@"dstImage" isEqualToString:_resizeCanvasTo]) {
     return dstExtent;
   }
-  
+
   if ([@"srcImage" isEqualToString:_resizeCanvasTo]) {
     return srcExtent;
   }
-  
+
   if ([@"MIN" isEqualToString:_resizeCanvasTo]) {
     return MIN(dstExtent, srcExtent);
   }
-  
+
   if ([@"MAX" isEqualToString:_resizeCanvasTo]) {
     return MAX(dstExtent, srcExtent);
   }
-  
+
   RCTAssert(false, @"ImageFilterKit: unknown resizeCanvasTo input - %@", _resizeCanvasTo);
-  
+
   return 0;
 }
 
@@ -171,7 +170,7 @@
   CIImage *dstImage = ((CIImage *)[self inputs][[self dstImageName]][@"image"]);
   CGRect srcFrame = srcImage.extent;
   CGRect dstFrame = dstImage.extent;
-  
+
   CGSize outSize = CGSizeMake(
     [self canvasExtentWithDstExtent:dstFrame.size.width
                           srcExtent:srcFrame.size.width
@@ -180,33 +179,32 @@
                           srcExtent:srcFrame.size.height
                       defaultExtent:_canvasSize.height]
   );
-  
+
   [self initFilter:outSize];
-  
+
   CGAffineTransform dstTransform = [IFKCompositionPostProcessor
                                     imageTransformWithCanvasWidth:outSize.width
                                     canvasHeight:outSize.height
                                     imageWidth:dstFrame.size.width
                                     imageHeight:dstFrame.size.height
                                     transform:_dstTransform];
-  
+
   CGAffineTransform srcTransform = [IFKCompositionPostProcessor
                                     imageTransformWithCanvasWidth:outSize.width
                                     canvasHeight:outSize.height
                                     imageWidth:srcFrame.size.width
                                     imageHeight:srcFrame.size.height
                                     transform:_srcTransform];
-  
+
   [[self filter] setValue:[srcImage imageByApplyingTransform:srcTransform]
                    forKey:_swapImages ? [self dstImageName] : @"inputImage"];
-  
+
   [[self filter] setValue:[dstImage imageByApplyingTransform:dstTransform]
                    forKey:_swapImages ? @"inputImage" : [self dstImageName]];
-  
-  return [self filteredImageWithScale:image.scale
-                           resizeMode:resizeMode
-                            viewFrame:CGRectMake(0, 0, outSize.width, outSize.height)
-                             destSize:outSize];
+
+  return [self filteredImage:image
+                  resizeMode:resizeMode
+                   viewFrame:CGRectMake(0, 0, outSize.width, outSize.height)];
 }
 
 @end
